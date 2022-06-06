5c3ac61299c2d61bb9869904fbce45a813312487
Ruslan.Belous <r.bilous@viseven.com>
2022-06-06 15:55:03 +0300

    update(*):added deploy files

--- src/App.vue
+++ src/App.vue
@@ -28,6 +28,7 @@ body {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100%;
   margin-top: 60px;
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
-module.export = {
-    publicPath: `/Mortgage_calculator/`
-} 
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/mortgage_calculator/'
+        : '/'
+}
